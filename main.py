64ae27c99f6b393fb497d663e4044cef195a9253
Kirill <Kirill.Teplekov@yandex.ru>
2019-04-23 15:51:29 +0300

    Fix handle dialog, add showing town on the map

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from flask import Flask, request
+import requests
 import logging
 import json
 from geo import get_distance_on_map, get_country, get_toponym
@@ -33,43 +34,56 @@ def main():
 def handle_dialog(res, req):
     user_id = req['session']['user_id']
     help_words = ['помощь', 'документация']
+    map_type_words = ['спутник', 'карта', 'гибрид']
 
-    cities = get_cities(req)
+    original_utterance = req['request']['original_utterance'].lower()
 
+    # Если пользователь новый, то предоставляем ему информацию о навыке и просим представиться
     if req['session']['new']:
         res['response']['text'] = "Здравствуйте данный навык позволяет получить некоторые геоданные, " \
                                   "используя API Яндекс.Карт, а также фотографии городов. " \
-                                  "Напишите 'помощь', чтобы получить полную информацию о работе навыка. " \
+                                  "Напишите 'помощь' или 'документация', " \
+                                  "чтобы получить полную информацию о работе навыка. " \
                                   "Но для начала, прошу вас представиться."
         sessionStorage[user_id] = {
             'first_name': None
         }
-
-    elif sessionStorage[user_id]['first_name'] is None:
+        return
+    if sessionStorage[user_id]['first_name'] is None:
+        # Получаем первую именнованую сущность
         first_name = get_first_name(req)
+        # Если не обнаружено - просим повторить
         if first_name is None:
             res['response']['text'] = \
                 'Не расслышала имя. Повтори, пожалуйста!'
+        # Иначе записываем имя в словарь и обращаемся к пользователю по имени
         else:
             sessionStorage[user_id]['first_name'] = first_name
             res['response']['text'] = 'Приятно познакомиться, ' + first_name.title() + '. Я - Алиса. Чем займемся?'
+        return
 
-    elif req['request']['original_utterance'] in help_words:
-        res['response']['text'] = "Данный навык работает с API Яндекс.Карт."
+    if original_utterance in help_words:
+        res['response']['text'] = "Данный навык работает с API Яндекс.Карт..."
+        return
 
-    elif req['request']['original_utterance']:
-        pass
+    # Узнаем тип карты
+    if original_utterance in map_type_words:
+        for word in original_utterance:
+            if word in map_type_words:
+                map_type = word
+    else:
+        map_type = 'карта'
 
-    elif cities:
-        if len(cities) == 0:
-            res['response']['text'] = 'Ты не написал название не одного города!'
-        elif len(cities) == 1:
+    # Получаем список  всех городов во фразе
+    cities = get_cities(req)
+    if cities:
+        # Если город один, то называем страну, в которой он находится
+        if len(cities) == 1:
             res['response']['text'] = 'Этот город в стране - ' + get_country(cities[0])
+        # Если же во фразе два городо, то вычисляем расстояние между городами и отмечаем их на карте
         elif len(cities) == 2:
             image, distance = get_distance_on_map(cities[0], cities[1], map_type)
             res['response']['text'] = 'Расстояние между этими городами: ' + distance + ' км.'
-        else:
-            pass
         return
 
 
@@ -90,18 +104,25 @@ def get_cities(req):
     return cities
 
 
+# Загружаем изображение на Яндекс.Диалоги
 def post_image(image):
-    map_file = "map.png"
+    skill_id = "f268642e-e326-4e68-8516-46ea1dfaa8e3"
+    token = "AQAAAAAgSPKYAAT7o5rTzHsWUUMDm2-biDwcrFs"
+    file_name = "map.png"
+
     try:
-        with open(map_file, "wb") as file:
+        with open(file_name, "wb") as file:
             file.write(image)
     except IOError as ex:
         print("Ошибка записи временного файла:", ex)
         sys.exit(2)
+
     url = f'https://dialogs.yandex.net/api/v1/skills/{skill_id}/images'
-    files = {'file': open(map_file, 'rb')}
+    files = {'file': open(file_name, 'rb')}
     headers = {'Authorization': f'OAuth {token}'}
-    s = post(url, files=files, headers=headers)
+    response = requests.get(url, files=files, headers=headers)
+    image_id = response.json['image']['id']
+    return image_id
 
 
 def show_photo(city):
